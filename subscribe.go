3cc64254bf491a78fe841f73ea993cc270ebaa6b
amuslija <muslija.adnan@gmail.com>
2024-03-19 11:54:24 +0100

    chore: more logging

--- subscribe.go
+++ subscribe.go
@@ -153,6 +153,7 @@ func (c *client) loop(messageChan <-chan paho.Message, timeout uint) {
 
 // AddEventListener expose the js method to listen for events
 func (c *client) AddEventListener(event string, listener func(goja.Value) (goja.Value, error)) {
+	println("adding listener for", event)
 	switch event {
 	case "message":
 		c.messageListener = listener
